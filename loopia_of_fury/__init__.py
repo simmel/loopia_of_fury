4effae85ee3ab91997c0f9da619f846f1a9b3a3b
Simon Lundström <github-commits@soy.se>
2021-02-10 09:28:03 +0100

    Implement parse_args
    Green bar!

--- loopia_of_fury/__init__.py
+++ loopia_of_fury/__init__.py
@@ -1,4 +1,11 @@
 __version__ = "0.1.0"
+import argparse
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="test")
+    args = parser.parse_args()
+    return args
 
 
 def main() -> None:
