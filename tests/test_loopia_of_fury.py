bd5bf597509f17d039b95b684be9014b1b51f87a
Simon Lundström <github-commits@soy.se>
2021-02-10 09:23:24 +0100

    Add tsts for parsing pargs

--- tests/test_loopia_of_fury.py
+++ tests/test_loopia_of_fury.py
@@ -1,5 +1,10 @@
-from loopia_of_fury import __version__
+from loopia_of_fury import __version__, argparse, parse_args
 
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+def test_parse_args():
+    args = parse_args()
+    assert type(args) == argparse.Namespace
