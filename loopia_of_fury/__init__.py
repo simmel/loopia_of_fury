581f6ae600e566b81cf5da8b74af36b9e2292201
Simon Lundström <github-commits@soy.se>
2021-02-20 08:04:17 +0100

    Add password argument

--- loopia_of_fury/__init__.py
+++ loopia_of_fury/__init__.py
@@ -8,6 +8,10 @@ def parse_args(
     argv: Optional[Sequence[str]] = sys.argv[1:],
 ) -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="test")
+    parser.add_argument(
+        "--password",
+    )
+
     args = parser.parse_args(args=argv)
     return args
 
